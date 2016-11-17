f1ed733713f56a24a805cc634c017d26f714940e
Katrina Tollin <ktollin@gmail.com>
2016-11-17 13:49:13 -0600

    Change associations to make UserGames accessible

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,12 +1,12 @@
 class Game < ApplicationRecord
   has_many :user_games
   has_many :users, through: :user_games
-  validates :winner, presence: true
-  # validate :only_two_players
+  # validates :winner, presence: true
+  validate :only_two_players
 
-  # private
+  private
 
-  # def only_two_players
-  #   self.users.length === 2
-  # end
+  def only_two_players
+    self.users.length === 2
+  end
 end
--- app/models/user_game.rb
+++ app/models/user_game.rb
@@ -1,3 +1,5 @@
 class UserGame < ApplicationRecord
+  belongs_to :game
+  belongs_to :user
   validates :game_id, :user_id, presence: true
 end
