93511ccdffe1c4b767b57c309ef89a8389231fc5
Matt McGee <matthew.m.mcgee@gmail.com>
2016-11-17 17:49:02 -0600

    Write logic for isPlaceable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,105 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var coordinates;
+
+$(document).ready(function() {
+	
+	$('td').on('click', function(){ 
+		var column = $(this).attr("class");
+		var row = $(this).closest('tr').attr('id');
+		return coordinates = [column, parseInt(row)]
+	});
+
+
+});
+
+
+
+
+
+
+
+
+
+
+var Game = function(){
+
+
+}
+
+
+var Ship = function(name, length){
+	this.name = name;
+	this.length = length;
+	this.orientation = "horizontal";
+}
+
+
+var patrolBoat = new Ship('patrol boat', 2)
+var submarine = new Ship('submarine', 3)
+var destroyer = new Ship('destroyer', 3)
+var battleShip = new Ship('battleship', 4)
+var aircraftCarrier = new Ship('aircraft carrier', 5)
+
+var shipChoices = [patrolBoat, submarine, destroyer, battleShip, aircraftCarrier]
+
+
+
+Ship.prototype.changeOrientation = function(){
+	if (this.orientation == "horizontal") {
+		return this.orientation = "vertical"
+	} else if (this.orientation == "vertical") {
+		return this.orientation = "horizontal"
+	};
+};
+
+Ship.prototype.isPlaceable = function(x, y){
+	if (this.orientation == "vertical"){
+		if((y + this.length -1) <= 10){
+			console.log('Yes, is placeable!')
+			return true
+		}
+		else {
+			console.log("Nope. Not placeable.")
+			return false	
+		}
+	} else if (this.orientation == "horizontal"){
+		x = x.charCodeAt(0) - 64;
+		if((x + this.length -1) <= 10){
+			console.log("Yes, is placeable!")
+			return true;
+		}
+		else {
+			console.log("Nope. Not placeable.")
+			return false
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
